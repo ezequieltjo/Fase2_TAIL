4dce651c6d5e3dc5537117568dcd0285c675b32e
ezequieltjo <ezequieltjo@gmail.com>
2021-03-19 17:45:22 -0300

    init exec2.py

---
+++ exec2.py
@@ -0,0 +1,6 @@
+"""
+    Exercício 2 - Fase 2
+"""
+import pandas as pd
+dataset = pd.read_csv('https://raw.githubusercontent.com/franklinthony/dataset/master/titanic_disaster.csv', sep = ',')
+print(dataset)
